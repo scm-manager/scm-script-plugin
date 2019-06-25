4ad55583a0eda8b79914539c396d20cb488d1a6e
Florian Scholdei <florian.scholdei@cloudogu.com>
2019-06-25 16:22:03 +0200

    used tabs instead of navigation

--- src/main/js/RootPage.js
+++ src/main/js/RootPage.js
@@ -1,16 +1,22 @@
 //@flow
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 import { translate } from "react-i18next";
-import { Page, Loading, ErrorNotification } from "@scm-manager/ui-components";
+import {
+  Loading,
+  ErrorNotification,
+  Title,
+  Subtitle
+} from "@scm-manager/ui-components";
 import { findAllScriptLinks } from "./api";
 import type { ScriptLinks } from "./types";
-import Main from "./scripts/Main";
 import ScriptRoot from "./script/ScriptRoot";
+import MainRouting from "./scripts/MainRouting";
 
 type Props = {
   link: string,
   // context props
+  location: any,
   t: string => string
 };
 
@@ -19,6 +25,22 @@ type State = {
   error?: Error,
   links?: ScriptLinks
 };
+
+export function isUrlSuffixMatching(
+  baseURL: string,
+  url: string,
+  suffix: string
+) {
+  let strippedUrl = url.substring(baseURL.length);
+  if (strippedUrl.startsWith("/")) {
+    strippedUrl = strippedUrl.substring(1);
+  }
+  const slash = strippedUrl.indexOf("/");
+  if (slash >= 0) {
+    strippedUrl = strippedUrl.substring(0, slash);
+  }
+  return strippedUrl === suffix;
+}
 
 class RootPage extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -54,9 +76,12 @@ class RootPage extends React.Component<Props, State> {
     } else if (links) {
       return (
         <>
-          <Route path="/scripts" component={() => <Main links={links} />} />
           <Route
-            path="/script/:id"
+            path="/admin/scripts"
+            component={() => <MainRouting links={links} />}
+          />
+          <Route
+            path="/admin/script/:id"
             component={() => <ScriptRoot links={links} />}
           />
         </>
@@ -66,17 +91,47 @@ class RootPage extends React.Component<Props, State> {
     }
   };
 
+  navigationClass(suffix: string) {
+    const { baseURL, location } = this.props;
+    if (location && isUrlSuffixMatching(baseURL, location.pathname, suffix)) {
+      return "is-active";
+    }
+    return "";
+  }
+
   render() {
     const { t } = this.props;
     const body = this.createBody();
 
+    let tabs = (
+      <div className="tabs">
+        <ul>
+          <li className={this.navigationClass("run")}>
+            <Link to="/admin/scripts/run">
+              {t("scm-script-plugin.navigation.run")}
+            </Link>
+          </li>
+          <li className={this.navigationClass("stored")}>
+            <Link to="/admin/scripts">
+              {t("scm-script-plugin.navigation.stored")}
+            </Link>
+          </li>
+          <li className={this.navigationClass("samples")}>
+            <Link to="/admin/scripts/samples">
+              {t("scm-script-plugin.navigation.samples")}
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+
     return (
-      <Page
-        title={t("scm-script-plugin.root-page.title")}
-        subtitle={t("scm-script-plugin.root-page.subtitle")}
-      >
+      <>
+        <Title title={t("scm-script-plugin.root-page.title")} />
+        <Subtitle subtitle={t("scm-script-plugin.root-page.subtitle")} />
+        {tabs}
         {body}
-      </Page>
+      </>
     );
   }
 }
