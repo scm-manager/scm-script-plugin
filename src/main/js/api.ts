67b0066b2330009adf484c85f28f99f94b20a5fe
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-11-20 10:45:03 +0100

    Fix type

--- src/main/js/api.ts
+++ src/main/js/api.ts
@@ -4,10 +4,8 @@ import { Links } from "@scm-manager/ui-types";
 
 // modifying headers is not supported in the apiclient
 export function run(link: string, language: string, content: string): Promise<ScriptExecutionResult> {
-  const headers = new Headers();
-  // @ts-ignore
+  const headers: Record<string, string> = {};
   headers["Accept"] = "application/vnd.scmm-script-execution-result+json;v=2";
-  // @ts-ignore
   headers["Cache"] = "no-cache";
   return apiClient.postText(link + "?lang=" + language, content, headers)
     .then(resp => resp.json());
